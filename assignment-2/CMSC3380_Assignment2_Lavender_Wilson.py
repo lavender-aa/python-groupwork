8a9e98055eadfac811ee676929d262293b482b72
lavender-aa <sodaissoda@gmail.com>
2025-02-12 19:09:33 -0500

    assignment 2 setup

---
+++ assignment-2/CMSC3380_Assignment2_Lavender_Wilson.py
@@ -0,0 +1,25 @@
+# Program: Assignment 2
+# Authors: Group #
+#   - Lavender Wilson
+#   - Nik Roebuck
+#   - Bri Dulik
+
+# notes:
+#   - use list comprehensions
+#   - data file is nonsense...?
+
+# todo:
+# 1. Read the three lists as list1, list2, and list3 and print them.
+# 2. Create and print list4 that consists of all odd numbers of list1.
+# 3. Create and print list5 that has all numbers of list2 after adding 2 to all numbers.
+# 4. Create and print list6 that has all numbers of list3 after adding 2 to all numbers greater than 18.
+# 5. Create and print list7 that has all the common numbers of list1 and list2.
+# 6. Create and print list8 that has all numbers of list3; however, they are not in list1 or list2.
+# 7. Create and print list9 that has all the uncommon numbers of list1 and list2.
+
+# main function
+def main():
+    pass
+
+# call main
+main()
