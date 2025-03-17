67a409ee0ecedca1beb85433ed4b43ee8daa30f5
lavender-aa <sodaissoda@gmail.com>
2025-03-17 10:15:53 -0400

    added notes

--- assignment-3/CMSC3380_Assignment3_8.py
+++ assignment-3/CMSC3380_Assignment3_8.py
@@ -4,6 +4,25 @@
 #       - Lavender Wilson
 #       - Nik Roebuck
 
+
+# notes:
+# 
+# - courses: dictionary (key=code, val=title)
+# - students: dictionary of dictionaries (use IDs as key for parent)
+
+# menu options:
+#
+# - add a course
+# - delete a course
+# - list all courses
+# - add a student
+# - delete a student
+# - list all students
+# - add a course to a student
+# - delete a course from a student
+# - list all student courses
+# - quit
+
 def main():
     pass
 
